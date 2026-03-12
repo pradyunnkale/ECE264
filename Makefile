b3cc1a43de6b9000ead6a8c04b141e0df354d751
Pradyunn Kale <kalepradyunn@gmail.com>
2026-03-12 10:30:46 -0400

    Finished THE1

---
+++ Makefile
@@ -0,0 +1,45 @@
+CFLAGS = -std=c99 -g -Wall -Wshadow --pedantic -Wvla -Werror
+GCC = gcc $(CFLAGS)
+EXEC = flesch
+
+all: main.c flesch.c
+	$(GCC) main.c flesch.c -o $(EXEC)
+
+testall: test1 test2 test3 test4 test5 test6 test7 test8
+
+test1: all
+	./$(EXEC) inputs/input_1.txt > output1.txt
+	diff output1.txt expected/expected_1.txt
+
+test2: all
+	./$(EXEC) inputs/input_2.txt > output2.txt
+	diff output2.txt expected/expected_2.txt
+
+test3: all
+	./$(EXEC) inputs/input_3.txt > output3.txt
+	diff output3.txt expected/expected_3.txt
+
+test4: all
+	./$(EXEC) inputs/input_4.txt > output4.txt
+	diff output4.txt expected/expected_4.txt
+
+test5: all
+	./$(EXEC) inputs/input_5.txt > output5.txt
+	diff output5.txt expected/expected_5.txt
+
+test6: all
+	./$(EXEC) inputs/input_6.txt > output6.txt
+	diff output6.txt expected/expected_6.txt
+
+test7: all
+	./$(EXEC) inputs/input_7.txt > output7.txt
+	diff output7.txt expected/expected_7.txt
+
+test8: all
+	./$(EXEC) inputs/input_8.txt > output8.txt
+	diff output8.txt expected/expected_8.txt
+
+clean:
+	rm -f $(EXEC)
+	rm -f *.o
+	rm -f output*
--- flesch.c
+++ flesch.c
@@ -9,23 +9,85 @@
 #include "flesch.h"
 
 int countSentences(const char* text) {
-    // TODO: Implement this function
-    return 0;
+    if (text[0] == '\0')
+    {
+        return 0;
+    }
+
+    int i, sentences;
+    i = 0;
+    sentences = 0;
+    while (text[i] != '\0')
+    {
+        if (text[i] == '.' || text[i] == ':' || text[i] == ';' || text[i] == '?' || text[i] == '!')
+        {
+            sentences++;
+        }
+        i++;
+    }
+
+    if (sentences == 0)
+    {
+        sentences = 1;
+    }
+
+    return sentences;
 }
 
 int countWords(const char* text) {
-    // TODO: Implement this function
-    return 0;
+    if (text[0] == '\0')
+    {
+        return 0;
+    }
+
+    int i, words;
+    words = 0;
+    i = 0;
+    while (text[i] != '\0')
+    {
+        if (!(text[i] == ' ' || text[i] == '.' || text[i] == ':' || text[i] == ';' || text[i] == '?' || text[i] == '!')
+            && (i == 0 || (text[i-1] == ' ' || text[i-1] == '.' || text[i-1] == ':' || text[i-1] == ';' || text[i-1] == '?' || text[i-1] == '!')))
+        {
+            words++;
+        }
+        i++;
+    }
+
+    return words;
 }
 
 int countSyllables(const char* text) {
-    // TODO: Implement this function
-    return 0;
+    if (text[0] == '\0')
+    {
+        return 0;
+    }
+
+    int prev_vowel;
+    int i, syllables;
+    i = 0;
+    syllables = 0;
+    prev_vowel = 0;
+    while (text[i] != '\0')
+    {
+        if (text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u' ||
+            text[i] == 'A' || text[i] == 'E' || text[i] == 'I' || text[i] == 'O' || text[i] == 'U') 
+        {
+            prev_vowel = 1;
+        } 
+        else if (prev_vowel)
+        {
+            syllables++;
+            prev_vowel = 0;
+        }
+        i++;
+    }
+    return syllables;
 }
 
 double calculateFleschScore(TextStats stats) {
-    // TODO: Implement this function
-    return 0.0;
+    double fleschScore;
+    fleschScore = 206.835 - (1.015 * ((float) stats.words / (float) stats.sentences)) - (84.6 * (float) stats.syllables / (float) stats.words);
+    return fleschScore;
 }
 
 TextStats analyzeText(const char* text) {
